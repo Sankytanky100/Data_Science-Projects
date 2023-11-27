d4dce9224a81880e44e8c0182d086a3fc49443af
Sankytanky100 <140347750+Sankytanky100@users.noreply.github.com>
2023-11-27 12:10:21 -0500

    Add files via upload

---
+++ _DataScienceEcosystem.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ##In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+# Objective
+# In this final assignment, you will:
+# 
+# Create a Jupyter Notebook
+# 
+# Insert code and markdown cells
+# 
+# Share your notebook through GitHub
+
+# In[ ]:
+
+
+Some of the popular languages that Data Scientists use are
+ordered_list_1 = ('python', 'java', 'R')
+
+
+# In[ ]:
+
+
+Some of the commonly used libraries used by Data Scientists include:
+ordered_list_2 = (NumPy,Pandas,Matplotlib)
+
+
+# ## Data Science Tools 
+# 
+
+# In[ ]:
+
+
+Jupyter Notebooks / JupyterLab
+
+
+# In[ ]:
+
+
+RStudio
+
+
+# In[ ]:
+
+
+Visual Studio Code (VS Code)
+
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[1]:
+
+
+(3*4)+5 #This a simple arithmetic expression to mutiply then add integers.
+
+
+# In[6]:
+
+
+200 // 60 #minutes into hours # This will convert 200 minutes to hours by diving by 60
+
+
+# ### Objectives:
+# popular_languages = [
+#     'Python',
+#     'R',
+#     'Julia',
+#     'SQL']
+
+# ## Author Lakshmi Holla, Niveditha Pandith
+# 
+# ## Other contributor(s) Rav Ahuja, Malika Singla
+# 
+# Name - Sankalp Tripathi
